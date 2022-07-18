a3e5fe576d9e2100264990ef0fb56348de1fc962
Francisco Foukal <franciscofoukal@gmail.com>
2022-07-18 19:03:04 -0300

    setup presenters

--- cmd/api/app/router.go
+++ cmd/api/app/router.go
@@ -4,13 +4,12 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/labscool/mb-appointment-system/cmd/api/presenter"
 )
 
 func InitRoutes(app *gin.Engine) {
 
 	app.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
+		c.JSON(http.StatusOK, presenter.PingResponse{Message: "pong"})
 	})
 }
---
+++ cmd/api/presenter/ping.go
@@ -0,0 +1,5 @@
+package presenter
+
+type PingResponse struct {
+	Message string `json:"message"`
+}
