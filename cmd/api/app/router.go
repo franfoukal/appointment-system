49ea51a68e9a82127a41aa46727abb2184cc82c5
Francisco Foukal <franciscofoukal@gmail.com>
2022-07-18 18:28:06 -0300

    setup router

--- cmd/api/app/router.go
+++ cmd/api/app/router.go
@@ -1,1 +1,16 @@
 package app
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func InitRoutes(app *gin.Engine) {
+
+	app.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+}
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/labscool/mb-appointment-system/cmd/api/app"
+)
+
+func main() {
+	r := gin.Default()
+
+	app.InitRoutes(r)
+
+	r.Run()
+}
